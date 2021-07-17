1551515221cdacb3c7891e37e9860107da939a22
monaqa <mogassy@yahoo.co.jp>
2021-07-17 09:30:59 +0900

    improve raf new command; add raf ls command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::path::{Path, PathBuf};
 
 use anyhow::Result;
 use chrono::{Date, Datelike, Local};
+use dialoguer::Input;
+use glob::glob;
 use raf::option::RafConfig;
 use structopt::StructOpt;
 
@@ -11,10 +13,13 @@ enum Args {
     /// Create a new draft file and output the path of it.
     New {
         /// project type.
-        kind: String,
+        #[structopt(short, long)]
+        kind: Option<String>,
         /// project slug.
-        slug: String,
+        #[structopt(short, long)]
+        slug: Option<String>,
     },
+    Ls,
 }
 
 fn main() -> Result<()> {
@@ -24,16 +29,37 @@ fn main() -> Result<()> {
     let raf_config_root = format!("{}/.raf/config.toml", std::env::var("HOME").unwrap());
     let raf_config_root = Path::new(&raf_config_root);
     let raf_config_root = std::fs::read_to_string(raf_config_root)?;
-    let raf_config : RafConfig = toml::from_str(&raf_config_root)?;
+    let raf_config: RafConfig = toml::from_str(&raf_config_root)?;
+    let raf_root = raf_config.path.root;
 
     let args = Args::from_args();
     let today = Local::today();
 
     match args {
         Args::New { kind, slug } => {
-            let raf_root = raf_config.path.root;
+            let kind: String = kind.unwrap_or_else(|| {
+                Input::new()
+                    .with_prompt("Project kind")
+                    .interact_text()
+                    .unwrap()
+            });
+            let slug: String = slug.unwrap_or_else(|| {
+                Input::new()
+                    .with_prompt("Project slug")
+                    .interact_text()
+                    .unwrap()
+            });
             let new_dir = create_new_dir(&raf_root, &kind, &today, &slug)?;
             println!("{}", new_dir.to_str().unwrap());
+        }
+        Args::Ls => {
+            let str_root = raf_root.to_str().unwrap();
+            let g = glob(&format!("{}/*/*/*/*/*", str_root))?;
+            for p in g {
+                if let Some(s) = p.ok().and_then(|p| p.to_str().map(|s| s.to_owned())) {
+                    println!("{}", s);
+                }
+            }
         }
     }
 
